565a551885498a1de04c0ea68b8eb474589c8c73
PkBertold <68712243+PkBertold@users.noreply.github.com>
2020-11-07 23:51:39 +0100

    Update jokes.js

--- js/jokes.js
+++ js/jokes.js
@@ -1,1 +1,13 @@
+let btn = document.getElementById('btn');
+let output = document.getElementById('output');
+let quotes = [
+    'Miért nem vesz a rendőr zsebszámológépet? Mert fejből is tudja, hogy hány zsebe van...', 
+    'Hogy hívják az erdőgazdálkodási informatikust? Fakocka!', 
+    'Hol terem a CD ROM? DISCréten.',
+    'Mit csinál a kaméleon a sütőben? Beleolvad a környezetébe. ',
+];
 
+btn.addEventListener('click', function(){
+    var randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+    output.innerHTML = randomQuote;
+})
